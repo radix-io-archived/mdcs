0fdccd2da1c1d442f2700e7428a7d30592ffb264
Matthieu Dorier <mdorier@anl.gov>
2017-09-11 08:40:15 -0500

    added a test

---
+++ test/test_server.c
@@ -0,0 +1,106 @@
+#include <assert.h>
+#include <stdio.h>
+#include <margo.h>
+#include <mdcs/mdcs-service.h>
+#include <mdcs/mdcs-counters.h>
+#include "types.h"
+
+/* after serving this number of rpcs, the server will shut down. */
+static const int TOTAL_RPCS = 16;
+/* number of RPCS already received. */
+static int num_rpcs = 0;
+
+static mdcs_counter_t mycounter = MDCS_COUNTER_NULL;
+
+/* 
+ * hello_world function to expose as an RPC.
+ * This function just prints "Hello World"
+ * and increment the num_rpcs variable.
+ *
+ * All Mercury RPCs must have a signature
+ *   hg_return_t f(hg_handle_t h)
+ */
+hg_return_t sum(hg_handle_t h);
+DECLARE_MARGO_RPC_HANDLER(sum)
+
+/*
+ * main function.
+ */
+int main(int argc, char** argv)
+{
+	/* Initialize Margo */
+	margo_instance_id mid = margo_init("bmi+tcp://localhost:1234", MARGO_SERVER_MODE, 0, 0);
+    assert(mid);
+
+	/* Register the RPC by its name ("sum") */
+	MARGO_REGISTER(mid, "sum", sum_in_t, sum_out_t, sum);
+
+	int ret;
+	
+	ret = mdcs_init(mid, MDCS_TRUE);
+	assert(ret == MDCS_SUCCESS);
+
+	ret = mdcs_counter_register("example:mycounter", MDCS_COUNTER_LAST_INT64, 0, &mycounter); 
+
+	/* NOTE: there isn't anything else for the server to do at this point
+     * except wait for itself to be shut down.  The
+     * margo_wait_for_finalize() call here yields to let Margo drive
+     * progress until that happens.
+	 */
+	margo_wait_for_finalize(mid);
+
+	mdcs_finalize();
+
+	return 0;
+}
+
+/* Implementation of the RPC. */
+hg_return_t sum(hg_handle_t h)
+{
+	hg_return_t ret;
+	num_rpcs += 1;
+
+	sum_in_t in;
+	sum_out_t out;
+
+	margo_instance_id mid = margo_hg_handle_get_instance(h);
+
+	/* Deserialize the input from the received handle. */
+	ret = margo_get_input(h, &in);
+	assert(ret == HG_SUCCESS);
+
+	/* Compute the result. */
+	out.ret = in.x + in.y;
+	printf("Computed %d + %d = %d\n",in.x,in.y,out.ret);
+
+	int64_t v = out.ret;
+	int r;
+	r = mdcs_counter_push(mycounter, &v);
+	assert(r == MDCS_SUCCESS);
+	int64_t stored = 0;
+	r = mdcs_counter_value(mycounter, &stored);
+	printf("Stored counter value is %ld\n", stored);
+	assert(r == MDCS_SUCCESS);
+
+	ret = margo_respond(mid, h, &out);
+	assert(ret == HG_SUCCESS);
+
+	/* Free the input data. */
+	ret = margo_free_input(h, &in);
+	assert(ret == HG_SUCCESS);
+
+	/* We are not going to use the handle anymore, so we should destroy it. */
+	ret = margo_destroy(h);
+	assert(ret == HG_SUCCESS);
+
+	if(num_rpcs == TOTAL_RPCS) {
+		/* NOTE: we assume that the server daemon is using
+		 * margo_wait_for_finalize() to suspend until this RPC executes, so there
+		 * is no need to send any extra signal to notify it.
+		 */
+		margo_finalize(mid);
+	}
+
+	return HG_SUCCESS;
+}
+DEFINE_MARGO_RPC_HANDLER(sum)
